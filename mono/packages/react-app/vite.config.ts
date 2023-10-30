5a11712376c5ec2f2f8283d28d1c2b18008cc583
Ahsan <ahsan_zami@hotmail.com>
2023-10-30 19:26:13 +0500

    vercel json added

--- mono/packages/react-app/vite.config.ts
+++ mono/packages/react-app/vite.config.ts
@@ -1,17 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            external: ["react", "react-router", "react-router-dom", "react-redux"],
-            output: {
-                globals: {
-                    react: "React",
-                },
-            },
-        },
-    }
-})
+  plugins: [react()],
+});
