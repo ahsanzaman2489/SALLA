cb597add987313ed66a115405793ed5cb96e8f96
Ahsan <ahsan_zami@hotmail.com>
2023-10-30 19:15:27 +0500

    Vite config changed for react

--- mono/packages/react-app/vite.config.ts
+++ mono/packages/react-app/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: ["react", "react-router", "react-router-dom", "react-redux"],
+            output: {
+                globals: {
+                    react: "React",
+                },
+            },
+        },
+    }
 })
